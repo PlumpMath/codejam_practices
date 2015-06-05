7c3cd946fbde24d22b03f6f546fbae40e2956899
jcca <jccarlos.a@gmail.com>
2015-06-04 20:26:50 -0400

    Solved: Tic-Tac-Toe-Tomek

--- tic-tac-toe-tomek.scm
+++ tic-tac-toe-tomek.scm
@@ -1,18 +1,70 @@
 ;;; https://code.google.com/codejam/contest/2270488/dashboard
 (use-modules (ice-9 rdelim)
-             (ice-9 regex))
+             (ice-9 regex)
+             (srfi srfi-11))
 
+(define X #\X)
+(define O #\O)
+(define T #\T)
+(define DOT #\.)
+
+(define (line-won? who line)
+  (= 4 (length (filter (lambda (x) (or (equal? x who)
+                                  (equal? x T))) line)))) ; T is neutral
+
+(define (vertical-won? who tb)
+  (cond ((null? tb) #f)
+        ((line-won? who (car tb)) #t)
+        (else (vertical-won? who (cdr tb)))))
+
+(define (horizontal-tb tb)
+  (let*-values (((l1 l2 l3 l4) (apply values tb)))
+    (map (lambda (w x y z) (list w x y z)) l1 l2 l3 l4)))
+
+(define (horizontal-won? who tb)
+  (let loop ((tb (horizontal-tb tb)))
+    (cond ((null? tb) #f)
+          ((line-won? who (car tb)) #t)
+          (else (loop (cdr tb))))))
+
+(define (diagonal-tb1 tb)
+  (let loop ((i 0) (result '()) (tb tb))
+    (cond ((> i 3) result)
+          (else (loop (+ i 1) (cons (list-ref (car tb) i) result) (cdr tb))))))
+
+(define (diagonal-tb2 tb)
+  (let loop ((i 3) (result '()) (tb tb))
+    (cond ((< i 0) result)
+          (else
+           (loop (- i 1) (cons (list-ref (car tb) i) result) (cdr tb))))))
+
+(define (diagonal-won? who tb)
+  (cond ((line-won? who (diagonal-tb1 tb)) #t)
+        ((line-won? who (diagonal-tb2 tb)) #t)
+        (else #f)))
+
+(define (draw? tb)
+  (cond ((null? tb) #t)
+        ((< 0 (length (filter (lambda (x) (equal? x DOT)) (car tb)))) #f)
+        (else (draw? (cdr tb)))))
+
+;;; It can be more fast
 (define (solve-tomek tb)
-  "Draw")
+  (cond ((or (vertical-won? X tb) (horizontal-won? X tb)
+             (diagonal-won? X tb)) "X won")
+        ((or (vertical-won? O tb) (horizontal-won? O tb)
+             (diagonal-won? O tb)) "O won")
+        ;; Check empty or draw
+        ((draw? tb) "Draw")
+        (else "Game has not completed")))
 
 (define read-tomek
   (case-lambda
     ((in) (read-tomek in 0 '()))
     ((in i l) (cond ((< i 4)
                      (read-tomek in (+ i 1)
-                               (cons (string->list (read-line in)) l)))
+                                 (cons (string->list (read-line in)) l)))
                     (else (reverse l))))))
-
 
 (define (solve file-in file-out)
   (let* ((in (open-input-file file-in))
